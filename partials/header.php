028e96b9ac0c30534a9a4a252e16e31a59431108
Kundius Ruslan <kundius.ruslan@yandex.ru>
2025-06-15 15:46:29 +0300

    123

--- partials/header.php
+++ partials/header.php
@@ -3,10 +3,10 @@
     <div class="extra-header__city">
       <div class="city-select" data-city-select role="combobox" aria-expanded="false" aria-haspopup="true" aria-label="Выбор города">
         <button class="city-select__trigger" data-city-select-trigger>
-          <span><?php echo get_blog_details()->blogname; ?></span>
+          <span><?php echo get_blog_details(get_current_blog_id())->blogname; ?></span>
         </button>
         <div class="city-select__list" role="listbox" data-city-select-listbox>
-          <?php $sites = get_sites(); ?>
+          <?php $sites = get_sites([ 'site__not_in' => get_current_blog_id() ]); ?>
           <?php foreach ($sites as $site): ?>
           <a href="<?php echo $site->domain . $site->path; ?>" role="option" tabindex="-1">
             <?php echo get_blog_details($site->blog_id)->blogname; ?>
