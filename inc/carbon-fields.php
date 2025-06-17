3a172fd64625420807f6334349b49539c9cb56fc
Kundius Ruslan <kundius.ruslan@yandex.ru>
2025-06-17 20:05:16 +0300

    123

--- inc/carbon-fields.php
+++ inc/carbon-fields.php
@@ -303,6 +303,19 @@ function register_carbon_fields_blocks()
       ]);
     });
 
+  Block::make('user-review-list', 'Список "Отзывы"')
+    ->add_fields([
+      Field::make('separator', 'user-review-list', 'Список "Отзывы"'),
+    ])
+    ->set_category('layout')
+    ->set_mode('edit')
+    ->set_icon('shortcode')
+    ->set_render_callback(function ($fields, $attributes, $inner_blocks) {
+      get_template_part('partials/user-review-list', null, [
+        'fields' => $fields
+      ]);
+    });
+
   $theme_options_container = Container::make('theme_options', 'Параметры')
     ->add_tab('Общее', [
       Field::make('text', 'crb_theme_phone', 'Телефон')->set_help_text('Шорткод: {crb_theme_phone}'),
